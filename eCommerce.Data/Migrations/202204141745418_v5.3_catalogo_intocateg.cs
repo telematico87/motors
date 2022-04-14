1bdb1dab30045c49c498e0acf11d14514af07be6
FORTIZ <franccesco.waytech@waytech.pe>
2022-04-14 13:17:17 -0500

    edit de campos financiera

---
+++ eCommerce.Data/Migrations/202204141745418_v5.3_catalogo_intocateg.cs
@@ -0,0 +1,18 @@
+﻿namespace eCommerce.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v53_catalogo_intocateg : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "CatalogoID", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Categories", "CatalogoID");
+        }
+    }
+}
